930efe59cbefc3cf44d248f2fcd4032100822891
MohamedYusuf0 <moki2964@gmail.com>
2022-09-10 23:42:17 +0300

    done

---
+++ t/app/src/main/java/com/example/t/dev.java
@@ -0,0 +1,27 @@
+package com.example.t;
+
+public class dev{
+    private String st;
+    private int pr;
+
+    public dev(String st, int pr) {
+        this.st = st;
+        this.pr = pr;
+    }
+
+    public String getSt() {
+        return st;
+    }
+
+    public void setSt(String st) {
+        this.st = st;
+    }
+
+    public int getPr() {
+        return pr;
+    }
+
+    public void setPr(int pr) {
+        this.pr = pr;
+    }
+}
